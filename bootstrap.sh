8f86fd1498f72b972b7eaee4c8ecfcf78198829c
Carlo Wood <carlo@alinoe.com>
2020-01-13 19:46:31 +0100

    Use cwm4 for bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,2 @@
-libtoolize --force --automake
-aclocal -I m4/aclocal -I cwm4/aclocal
-autoheader
-automake --add-missing --foreign
-autoconf && sed -i 's/rm -f core/rm -f/' configure
+cwm4/scripts/bootstrap.sh
+sed -i 's/rm -f core/rm -f/' configure
